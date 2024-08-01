904c756f68d93a039bce6b7b69b64d48f8734cf2
Mohanaram35 <146609447+Mohanaram35@users.noreply.github.com>
2024-08-01 20:05:52 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const registerButton = document.querySelector('button');
+    registerButton.addEventListener('click', function() {
+        alert('Thank you for your interest! Registration will open soon.');
+    });
+});
